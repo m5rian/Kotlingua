49bd0c817654103bbdf8f7b3a063aa103fdac8a4
Marian <toni_kukoc@gmx.de>
2021-10-19 10:35:20 +0200

    :children_crossing: Made ArgumentBuilder#arg suspend

--- src/main/kotlin/com/github/m5rian/kotlingua/ArgumentBuilder.kt
+++ src/main/kotlin/com/github/m5rian/kotlingua/ArgumentBuilder.kt
@@ -5,5 +5,5 @@ package com.github.m5rian.kotlingua
  */
 class ArgumentBuilder {
     val args = mutableMapOf<String, Any>()
-    fun arg(key: String, value: Any) = run { args[key] = value }
+    suspend fun arg(key: String, value: Any) = run { args[key] = value }
 }
