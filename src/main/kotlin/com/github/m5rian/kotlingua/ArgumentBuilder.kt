2308632cb5f7d375d58cfaacee2fd54c3e1257e1
Marian <toni_kukoc@gmx.de>
2021-10-19 10:36:23 +0200

    :bulb: Removed unused comments & Added Suppress annotation for warnings

--- src/main/kotlin/com/github/m5rian/kotlingua/ArgumentBuilder.kt
+++ src/main/kotlin/com/github/m5rian/kotlingua/ArgumentBuilder.kt
@@ -1,9 +1,7 @@
 package com.github.m5rian.kotlingua
 
-/**
- *
- */
 class ArgumentBuilder {
     val args = mutableMapOf<String, Any>()
+    @Suppress("RedundantSuspendModifier", "unused")
     suspend fun arg(key: String, value: Any) = run { args[key] = value }
 }
